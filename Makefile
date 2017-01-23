8775e71e4626d342961d1aacf9496ecb16397388
dzencot <dzencot@gmail.com>
2017-01-23 14:12:50 +0500

    init

---
+++ Makefile
@@ -0,0 +1,8 @@
+install:
+	npm install
+
+lint:
+	npm run eslint ./
+
+test:
+	npm test
---
+++ __test__/test.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('project-lvl2', () => {
+  it('should work', () => {
+    assert.equal(1, 1);
+  });
+});
