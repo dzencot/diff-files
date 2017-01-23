4ee89bce806a81338dd7b58581df599e4da7e592
dzencot <dzencot@gmail.com>
2017-01-23 23:13:25 +0500

    add eslint

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,15 +1,15 @@
 // @flow
 /* eslint-disable no-console */
 
-const help = require('commander');
+const program = require('commander');
 
-help
+program
   .version('0.0.1')
-  .usage('[options] <first_config> <second_config>')
+  .arguments('<first_config> <second_config>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format');
 
 
-help.parse(process.argv);
+program.parse(process.argv);
 console.log('hello, world');
 
