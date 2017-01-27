f5df56970a7fc32999ad72dc06de6c8b3b783831
dzencot <dzencot@gmail.com>
2017-01-28 00:43:31 +0500

    add toJson

--- src/lib/toJson.js
+++ src/lib/toJson.js
@@ -3,17 +3,19 @@
 
 const toJson = (obj) => {
   const result = Object.keys(obj).reduce((acc, key) => {
-    const typeVal = key.slice(0, 1) === ' ' ? undefined : key.slice(0, 1);
+    const typeVal = key.slice(0, 1);
     const nameKey = key.slice(2);
-    const value = typeof obj[key] === 'object' && typeVal === undefined ? toJson(obj[key]) : obj[key];
-    acc[nameKey] = {
-      type: `'${typeVal}'`,
+    const value = typeof obj[key] === 'object' && typeVal === ' ' ? toJson(obj[key]) : obj[key];
+    const res = {
+      name: nameKey,
+      type: typeVal,
       data: value,
     };
+    acc.push(res);
     return acc;
-  }, {});
+  }, []);
   return result;
 };
 
-export default toJson;
+export default diff => JSON.stringify(toJson(diff), null, '  ');
 
